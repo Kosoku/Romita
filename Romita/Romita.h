13337a0b1004038303838a3bd4410ee6d9a8a4e4
William Towe <willbur1984@gmail.com>
2023-10-07 14:52:19 -0400

    Initial Commit

---
+++ Romita/Romita.h
@@ -0,0 +1,18 @@
+//
+//  Romita.h
+//  Romita
+//
+//  Created by William Towe on 10/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Romita.
+FOUNDATION_EXPORT double RomitaVersionNumber;
+
+//! Project version string for Romita.
+FOUNDATION_EXPORT const unsigned char RomitaVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Romita/PublicHeader.h>
+
+
